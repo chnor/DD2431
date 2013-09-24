1e8d19c43ef37c0d20db07956cdfb10fa18442f6
Christopher Norman <chnor@csc.kth.se>
2013-09-24 16:56:21 +0200

    Added RBF + plotting

--- Lab 2/svm.py
+++ Lab 2/svm.py
@@ -12,10 +12,18 @@ def linear(A, B):
 	return A.transpose() * B + 1
 def quadratic(A, B):
 	return numpy.power((A.transpose() * B + 1), 2)
+def RBF_1(A, B, sigma):
+    K = A.transpose() * B / sigma**2
+    #d = numpy.diag(K)
+    #n = len(d)
+    #K =- numpy.matrix(numpy.outer(numpy.ones(n), d)) / 2
+    #K =- numpy.matrix(numpy.outer(d, numpy.ones(n))) / 2
+    return numpy.exp(K)
 def RBF(sigma):
 	return lambda A, B: numpy.exp(-((A - B).transpose() * (A - B)) / (2*sigma**2))
 
-K = quadratic
+#K = quadratic
+K = lambda A, B: RBF_1(A, B, 4)
 
 assert len(sys.argv) >= 2
 
@@ -30,18 +38,17 @@ classA = [(random.normalvariate(-1.5, 1),
            1.0)
            for i in range(n_A)]
 
-classB = [(random.normalvariate(0.0, 0.5),
-           random.normalvariate(-5.0, 2.5),
+classB = [(random.normalvariate(-5.0, 0.5),
+           random.normalvariate(-7.0, 2.5),
            -1.0)
-           for i in range(n_B)]
+           for i in range(n_B)] + \
+         [(random.normalvariate(1.5, 2),
+           random.normalvariate(10.0, 1),
+           -1.0)
+           for i in range(n_A)]
 
 data = classA + classB
 random.shuffle(data)
-
-#pylab.hold(True)
-#pylab.plot([p[0] for p in classA], [p[1] for p in classA], 'bo')
-#pylab.plot([p[0] for p in classB], [p[1] for p in classB], 'ro')
-#pylab.show()
 
 X = numpy.matrix([[p[0] for p in data], [p[1] for p in data]])
 t = numpy.array([p[2] for p in data])
@@ -67,8 +74,10 @@ alpha = numpy.array(r['x'])
 hits = numpy.nonzero(numpy.absolute(alpha) > 1e-6)[0]
 print "Support vectors are ", hits
 print "with values: ", alpha[hits].transpose()
+ind = lambda Y: ((alpha[hits].transpose() * t[hits]) * Kernel(X[:, hits], Y, K))
 
-c = ((alpha[hits].transpose() * t[hits]) * Kernel(X[:, hits], X, K)) > 0
+#c = ((alpha[hits].transpose() * t[hits]) * Kernel(X[:, hits], X, K)) > 0
+c = ind(X) > 0
 
 print "Misclassifed: ", round(10000.0 * numpy.sum(c != (t == 1)) / len(data))/100, "%"
 
@@ -88,7 +97,7 @@ test_A = [(random.normalvariate(-1.5, 1),
            for i in range(t_A)]
 
 test_B = [(random.normalvariate(0.0, 0.5),
-           random.normalvariate(-5.0, 2.5),
+           random.normalvariate(-15.0, 2.5),
            -1.0)
            for i in range(t_B)]
 
@@ -98,7 +107,31 @@ random.shuffle(test)
 X_test = numpy.matrix([[p[0] for p in test], [p[1] for p in test]])
 t_test = numpy.array([p[2] for p in test])
 
-c_test = ((alpha[hits].transpose() * t[hits]) * Kernel(X[:, hits], X_test, K)) > 0
+#c_test = ((alpha[hits].transpose() * t[hits]) * Kernel(X[:, hits], X_test, K)) > 0
+c_test = ind(X_test)
 
 print "Misclassified in test set: ", round(10000.0*numpy.sum(c_test != (t_test == 1)) / len(test))/100, "%"
+
+sv = X[:, hits]
+sv_1 = (t[hits].transpose() > 0)
+sv_2 = (t[hits].transpose() < 0)
+
+pylab.hold(True)
+pylab.plot([p[0] for p in classA], [p[1] for p in classA], 'b.')
+pylab.plot([p[0] for p in classB], [p[1] for p in classB], 'r.')
+pylab.plot(sv[0, sv_1], sv[1, sv_1], 'bo')
+pylab.plot(sv[0, sv_2], sv[1, sv_2], 'ro')
+
+xs = numpy.arange(-5, 10, 0.05)
+ys = numpy.arange(-12, 15, 0.05)
+xx, yy = numpy.meshgrid(xs, ys)
+grid = numpy.matrix(numpy.vstack((xx.ravel(), yy.ravel())))
+zs = numpy.reshape(ind(grid), (len(ys), len(xs)))
+
+pylab.contour(xs, ys, zs,
+              levels = (-1, 0, 1),
+              colors = ('red', 'black', 'blue'),
+              linewidths = (1, 1, 1))
+
+pylab.show()
 
